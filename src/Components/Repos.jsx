c70a614562a2d97404f9c00022e700719b40b50b
brianlunch <blynch5@tcd.ie>
2019-11-16 22:11:21 +0000

    Almost finished Repo UI apart from graph

--- src/Components/Repos.jsx
+++ src/Components/Repos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Moment from 'react-moment';
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 const Repos = (props) => {
@@ -27,19 +28,72 @@ return (
          })}
       </div>
    </div>
-   <div class="col-9">
+   <div class="col-md-9 text-left">
       <div class="tab-content" id="nav-tabContent">
          <div class="tab-pane fade show" id="allrepos" role="tabpanel" aria-labelledby="list-homes-list">All Repos</div>
          {elements.map((value,index) => {
          return(
          <div class="tab-pane fade show" id={value} role="tabpanel" aria-labelledby="list-homes-list">
-
-          </div>  
+            <i></i>
+            <div class="row justify-content-left text-dark">
+               <h5>{value}<br></br><br></br></h5>
+            </div>
+            <div class="row justify-content-left text-dark">
+               <div class="col-md-4 text-left">
+                  <div class="row justify-content-left text-dark">
+                     <div class="col-md-3 text-right ">Size:</div>
+                     <div class="col-md-7 text-left ">{String((Number(props.repos[index].size) * .001).toFixed(3))+" Mb"}</div>
+                  </div>
+               </div>
+               <div class="col-md-4 text-left">
+                  <div class="row justify-content-left text-dark">
+                     <div class="col-md-4 text-left">Forks: </div>
+                     {props.repos[index].forks ? 
+                     <div class="col-md-7 text-left ">{props.repos[index].forks}</div>
+                     : 
+                     <div class="col-md-7 text-left ">0</div>
+                     }
+                  </div>
+               </div>
+               
+                  <div class="col-md-4 text-left">
+                  <div class="row justify-content-left text-dark">`
+                  <div class="col-md-4 text-right">Created:</div>
+                  {props.repos[index].created_at ? 
+         <div class="col-md-7 text-left border-right">{<Moment from={new Date()}>{props.repos[index].created_at}</Moment>}</div>
+      : null }`
+                  </div>
+               </div>
+            </div>
+            <div class="row justify-content-left text-dark">
+               <div class="col-md-4 text-left">
+                  <div class="row justify-content-left text-dark">
+                     <div class="col-md-3 text-right ">Size:</div>
+                     <div class="col-md-7 text-left ">{String((Number(props.repos[index].size) * .001).toFixed(3))+" Mb"}</div>
+                  </div>
+               </div>
+               <div class="col-md-4 text-left">
+                  <div class="row justify-content-left text-dark">
+                     <div class="col-md-4 text-right">Watchers: </div>
+                     {props.repos[index].watchers ? 
+                     <div class="col-md-7 text-left ">{props.repos[index].watchers}</div>
+                     : 
+                     <div class="col-md-7 text-left ">0</div>
+                     }
+                  </div>
+               </div>
+               <div class="row justify-content-left text-dark">
+                  <div class="col-md-4 text-left">
+                  </div>
+               </div>
+            </div>
+            <b></b>
+         </div>
          )
          })}
       </div>
    </div>
-   </div>
+</div>
 )
 }else{return null;}
 };
