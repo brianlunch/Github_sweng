9aed3d59b621162104aa8247cef3ebecf6c3ed74
brianlunch <blynch5@tcd.ie>
2019-11-16 16:41:18 +0000

    began adding to repo list pop out

--- src/Components/Repos.jsx
+++ src/Components/Repos.jsx
@@ -7,28 +7,43 @@ const elements =[];
 if(props.repos){
 //const repoNumber = Number(props.infoclean.public_repos); 
 //console.log(repoNumber);
-for(const i = 0; i < 1; i++){
+for(const i = 0; i < props.repos.length; i++){
 elements.push(props.repos[i].name);
 }
 
 return (
-<div>
-      <div class="col-md-3 text-left">
-        <div class="list-group" id="myList" role="tablist">
-            {elements.map((value,index) => {
-            return (<a class="list-group-item list-group-item-action" id="list-module1Y2-list" data-toggle="list" href="#list-ModuleName1Y2" role="tab" aria-controls="module1Y2">ModuleName1</a>)
-            })}
-         </div>
-      </div>
-      <div class="col-9">
-         <div class="tab-content">
-            {elements.map((value,index) => {
-            return 
-            (<div class="tab-pane fade show active " id="list-ModuleName1Y2" role="tabpanel" aria-labelledby="list-home-list">Ytttttttttttttttttttttttttttttttttttt	</div>)
-            })}
-         </div>
-      </div>
+<div class="row">
+  <div class="col-3">
+  <h5>Select a repo to analyse</h5>
+    <div class="list-group" id="list-tab" role="tablist">
+	<a class="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href="#allrepos" role="tab" aria-controls="home">All repos</a>
+	{elements.map((value,index) => {
+      return(<a class="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href={"#"+value} role="tab" aria-controls="home">{value}</a>)
+   })}
    </div>
+  </div>
+  <div class="col-9">
+    <div class="tab-content" id="nav-tabContent">
+	<div class="tab-pane fade show" id="allrepos" role="tabpanel" aria-labelledby="list-homes-list">All Repos</div>
+	{elements.map((value,index) => {
+      return(<div class="tab-pane fade show" id={value} role="tabpanel" aria-labelledby="list-homes-list">
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  </div>)
+	  })}
+    </div>
+  </div>
+</div>
 )
 }else{return null;}
 };
