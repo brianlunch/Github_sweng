c14c551f433836701a85a0ffc4fc72d5a628c655
brianlunch <blynch5@tcd.ie>
2019-11-17 14:39:40 +0000

    fixed prop naming issue

--- src/Components/Graph.jsx
+++ src/Components/Graph.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import axios from 'axios';
-import Moment from 'react-moment';
-import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
+
 const Graph = (props) => {
 const elements =[];
 if(props.repos){
-    for(const i = 0; i < props.repos.length; i++){
-        elements.push(props.repos[i].name);
-        }
+    
 
 return (
 <p>hi</p>
